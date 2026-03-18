0397d05990cb2e12cf8cc0f059c804d8c47c7ae5
manciolove <114284283+manciolove@users.noreply.github.com>
2026-03-18 13:00:26 +0100

    Aggiungere i file di progetto.

---
+++ matrice_trasporta/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace matrice_trasporta
+{
+    internal class Program
+    {
+        static void Stampa (int[,] m)
+        {
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    Console.Write($"[{m[i,j]}]");
+                }
+                Console.WriteLine();
+            }
+        }
+        
+        static int[,] Trasporta (int[,] m, int[,] M)
+        {
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    M[j, i] = m[i, j];
+                }
+            }
+            return M;
+        }
+        static void Main(string[] args)
+        {
+            int[,] m = { { 1, 2, 3 },{ 4, 5, 6 },{ 7, 8, 9 } };
+            int[,] M = new int[3, 3];
+            Stampa(m);
+            Console.WriteLine("---------");
+            Stampa(Trasporta(m, M));
+
+        }
+    }
+}
---
+++ trasporta_non_quadrata/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace trasporta_non_quadrata
+{
+    internal class Program
+    {
+        static void Stampa(int[,] m)
+        {
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    Console.Write($"[{m[i, j]}]");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static int[,] Trasporta(int[,] m, int[,] M)
+        {
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    if (m.GetLength(0) == M.GetLength(1))
+                    {
+                        M[j, i] = m[i, j];
+                    }
+                }
+            }
+            return M;
+        }
+        static void Main(string[] args)
+        {
+            int[,] m = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+            int[,] M = new int[m.GetLength(1), m.GetLength(0)];
+            Stampa(m);
+            Console.WriteLine("---------");
+            Stampa(Trasporta(m, M));
+
+        }
+    }
+}
+
+
